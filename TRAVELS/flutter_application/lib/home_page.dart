dfc1781a1dca16b071494b967b4e2ab35ab32db3
Juan Victor <juanvictorqwerty@gmail.com>
2024-12-25 23:52:03 +0100

    done the homescreen

--- TRAVELS/flutter_application/lib/home_page.dart
+++ TRAVELS/flutter_application/lib/home_page.dart
@@ -5,9 +5,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: ElevatedButton(
-      onPressed:(){},
-        child: const Text("Reserve bus")),
-        );
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Center the buttons vertically
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.blue, // Text color
+            ),
+            child: const Text("Reserve bus"),
+          ),
+          const SizedBox(height: 30), // Space between buttons
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.green, // Text color
+            ),
+            child: const Text("Reserve plane"),
+          ),
+          const SizedBox(height: 30), // Space between buttons
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.red, // Text color
+            ),
+            child: const Text("Reserve hotel"),
+          ),
+        ],
+      ),
+    );
   }
-} 
+}
