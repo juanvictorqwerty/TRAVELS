206b1e138e722fe2772fbc2faead0642d51d0120
Juan Victor <juanvictorqwerty@gmail.com>
2024-12-27 19:41:44 +0100

    connect the google maps api

--- TRAVELS/flutter_application/lib/bus_reservation.dart
+++ TRAVELS/flutter_application/lib/bus_reservation.dart
@@ -1,10 +1,43 @@
-import 'package:flutter/material.dart'; 
+      import 'package:flutter/material.dart';
+      import 'package:google_maps_flutter/google_maps_flutter.dart';
+      
+      class BusReservation extends StatefulWidget {
+        const BusReservation({super.key});
+      
 
-class bus_reservation extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
+        @override
+        State<BusReservation> createState() => _BusReservationState();
+      }
+      
 
-}
+      class _BusReservationState extends State<BusReservation> {
+        late GoogleMapController mapController;
+      
+
+        final LatLng _center = const LatLng(-33.86, 151.20);
+      
+
+        void _onMapCreated(GoogleMapController controller) {
+          mapController = controller;
+        }
+      
+
+        @override
+        Widget build(BuildContext context) {
+          return MaterialApp(
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('Maps Sample App'),
+                backgroundColor: Colors.green[700],
+              ),
+              body: GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+              ),
+            ),
+          );
+        }
+      }
