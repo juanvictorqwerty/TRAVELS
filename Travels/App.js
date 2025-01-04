f957a17e444fe281dccaa6079033cfc0818f83ca
Juan Victor <juanvictorqwerty@gmail.com>
2025-01-04 23:15:40 +0100

    Importing the map screen

--- Travels/App.js
+++ Travels/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import BusCompaniesListing from './Screens/BusCompaniesListing';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <BusCompaniesListing/>
       <StatusBar style="auto" />
     </View>
   );
