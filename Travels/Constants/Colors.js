653f59bf8ab696f0e0e3ee61453980520e5ebac3
Juan Victor <juanvictorqwerty@gmail.com>
2025-01-05 17:41:19 +0100

    Whatever

---
+++ Travels/Constants/Colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+    white : '#FFFFFF',
+    dark: '#000000',
+    primary: '#3498db',
+    secondary: '#F7F7F7',
+    light : '#F2F2F2',
+    blue : '#3498db',
+}
+
+export default COLORS;
