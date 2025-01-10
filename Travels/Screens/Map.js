81e9ab368114cd689ccd316b2778583e622fac13
Juan Victor <juanvictorqwerty@gmail.com>
2025-01-10 11:00:50 +0100

    Welcome back to maps

--- Travels/Screens/Map.js
+++ Travels/Screens/Map.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-const Map = () => {
+export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Map</Text>
-        </View>
+    <View style={styles.container}>
+        <MapView style={styles.map} />
+    </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff', // Optional: Set a background color
+    flex: 1,
+    },
+    map: {
+    width: '100%',
+    height: '100%',
     },
 });
-
-export default Map;
